974ff11a98eaae266c457a6b94c22103f5f073e6
Berger1994 <LukasBerger1994@gmail.com>
2015-04-19 18:56:02 +0200

    Erster Versuch für den Verbindungsaufbau in Android

--- app/src/main/java/com/aause2s15/gruppe3/cheatingandroid/CreateMatchActivity.java
+++ app/src/main/java/com/aause2s15/gruppe3/cheatingandroid/CreateMatchActivity.java
@@ -1,19 +1,63 @@
 package com.aause2s15.gruppe3.cheatingandroid;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.apache.http.conn.util.InetAddressUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class CreateMatchActivity extends ActionBarActivity {
+
+    private static final int REQUEST_ENABLE_BT = 10;
+
+    private Thread serverThread;
+    private ServerSocket serverSocket;
+    private Handler updateConversationHandler;
+    private ArrayAdapter<String> adapter;
+
+
+    ArrayList<String> listItems=new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_match);
+
+        this.updateConversationHandler = new Handler();
+        this.serverThread = new Thread(new ServerThread());
+        this.serverThread.start();
+
+        final ListView listview = (ListView) findViewById(R.id.listView);
+
+        adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        listview.setAdapter(adapter);
     }
 
 
@@ -39,9 +83,81 @@ public class CreateMatchActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /** Called when the user clicks the start test match button */
-    public void startMatch (View view) {
+    /**
+     * Called when the user clicks the start test match button
+     */
+    public void startMatch(View view) {
         Intent intent = new Intent(this, MatchActivity.class);
         startActivity(intent);
     }
+
+    class ServerThread implements Runnable {
+        public void run() {
+            Socket socket = null;
+            try {
+                serverSocket = new ServerSocket(20201);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    socket = serverSocket.accept();
+
+                    updateConversationHandler.post(new updateUIThread("new socket connected"));
+                    CommunicationThread commThread = new CommunicationThread(socket);
+                    new Thread(commThread).start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    class CommunicationThread implements Runnable {
+
+        private Long id;
+        private Socket clientSocket;
+        private BufferedReader input;
+
+        public CommunicationThread(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+
+            try {
+                this.input = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    String read = input.readLine();
+
+                    if(read != null) {
+                        //updateConversationHandler.post(new updateUIThread(read));
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    class updateUIThread implements Runnable {
+        private String msg;
+
+        public updateUIThread(String str) {
+            this.msg = str;
+        }
+
+        @Override
+        public void run() {
+            listItems.add(msg);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
 }
