cc7275382405fa9aa631f3e70b7198b8b650e051
Berger1994 <LukasBerger1994@gmail.com>
2015-04-19 15:55:47 +0200

    test

--- app/src/main/java/com/aause2s15/gruppe3/cheatingandroid/MainActivity.java
+++ app/src/main/java/com/aause2s15/gruppe3/cheatingandroid/MainActivity.java
@@ -21,10 +21,6 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void test() {
-
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
