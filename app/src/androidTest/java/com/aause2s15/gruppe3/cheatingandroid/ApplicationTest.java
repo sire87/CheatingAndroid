841a0eb22c7f797c7af094de54dc125a12da2381
besapetschni <berni.sape@gmx.net>
2015-06-03 15:44:00 +0200

    activity Testing test (not running)

--- app/src/androidTest/java/com/aause2s15/gruppe3/cheatingandroid/ApplicationTest.java
+++ app/src/androidTest/java/com/aause2s15/gruppe3/cheatingandroid/ApplicationTest.java
@@ -1,7 +1,10 @@
 package com.aause2s15.gruppe3.cheatingandroid;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
 import android.test.ApplicationTestCase;
+import android.widget.Button;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -9,5 +12,17 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+       // Context.startActivity();
+
+        }
+    protected void setUp() throws Exception {
+        //super.setUp();
+       // mLaunchIntent = new Intent(getInstrumentation()
+       //         .getTargetContext(), LaunchActivity.class);
+       // startActivity(mLaunchIntent, null, null);
+       // final Button launchNextButton =
+         //       (Button) getActivity()
+        //                .findViewById(R.id.launch_next_activity_button);
     }
-}
+
+    }
